c5953224e2a8ddb949cf9843c5a775c3a458b8db
unknown <al196127@alumnos.uacj.mx>
2025-02-26 08:36:03 -0700

    [AGREGA] una base de la interfaz con diseño sencillo

--- app/src/main/java/mx/alumnos/uacj/example/la_calculadora/MainActivity.kt
+++ app/src/main/java/mx/alumnos/uacj/example/la_calculadora/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import mx.alumnos.uacj.example.la_calculadora.ui.theme.La_CalculadoraTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,22 +29,67 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             La_CalculadoraTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Calculadora()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun Calculadora() {
+    Column(modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center){
+        Text(text = "Aqui van numeritos", modifier =  Modifier
+            .padding(10.dp)
+            .fillMaxWidth()
+            .background(Color.Blue)
+            .height(50.dp),
+            textAlign = TextAlign.Right
+        )
+
+        // Deberia jugar mas con el estilo de aqui
+        Column (modifier = Modifier.fillMaxSize().background(Color.DarkGray)) {
+            Row (horizontalArrangement = Arrangement.SpaceAround,
+                modifier = Modifier.fillMaxWidth()){
+                Boton("9")
+                Boton("6")
+                Boton("3")
+            }
+
+            Row (horizontalArrangement = Arrangement.SpaceAround,
+                modifier = Modifier.fillMaxWidth()){
+                Boton("8")
+                Boton("5")
+                Boton("3")
+            }
+
+            Row (horizontalArrangement = Arrangement.SpaceAround,
+                modifier = Modifier.fillMaxWidth()){
+                Boton("7")
+                Boton("4")
+                Boton("1")
+            }
+
+            Row (horizontalArrangement = Arrangement.SpaceAround,
+                modifier = Modifier.fillMaxWidth()){
+                Boton(".")
+                Boton("0")
+                Boton("op")
+            }
+        }
+    }
+
+}
+
+@Composable
+fun Boton(etiqueta: String){
+    Text(etiqueta, modifier = Modifier
+        //.background(Color.Green)
+        .padding(25.dp)
+        .background(Color.Green)
+        .height(50.dp)
+        .width(50.dp),
+        textAlign = TextAlign.Center
     )
 }
 
@@ -42,6 +97,14 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     La_CalculadoraTheme {
-        Greeting("Android")
+        Calculadora()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun mostrar_boton(){
+    La_CalculadoraTheme {
+        Boton("4")
     }
 }
